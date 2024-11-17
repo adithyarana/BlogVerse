5ddc205a889fe3236e6be17051bae5662842ae1a
adithya <adithyarana447@gmail.com>
2024-11-17 15:13:55 +0530

    changes

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "./container";
+import Container from "./container/Container.jsx";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
